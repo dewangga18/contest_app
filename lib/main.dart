24cd1c6ffb850373f97eef420f543a0732fa23ce
Aaron <aaronevanjulio18@gmail.com>
2022-06-08 18:14:42 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:contest_app/my_detail_page.dart';
+import 'package:contest_app/my_home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => const MyHomePage()),
+        GetPage(name: '/detail', page: () => const DetailPage())
+      ],
+    );
+  }
+}
